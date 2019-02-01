dd9359bb28019b80e818c4160c81f1a1c4286bb6
lorenzopemac <lorenzopemac@gmail.com>
2019-02-01 11:12:21 -0300

    Pantalla principal a el Login

--- app/src/main/java/labprog/rompecabezasandroid/MainActivity.java
+++ app/src/main/java/labprog/rompecabezasandroid/MainActivity.java
@@ -1,7 +1,10 @@
 package labprog.rompecabezasandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
     }
+    public Button but1;
+
+    public void init(){
+        but1 = (Button) findViewById(R.id.button1);
+        but1.setOnClickListener(new  View.OnClickListener(){
+            public void onClick(View v){
+                Intent toy;
+                toy = new Intent(MainActivity.this , Login.class);
+                startActivity(toy);
+            }
+        });
+    }
+
+
+
 }
