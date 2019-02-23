57482870628a74adc58b5a07434a91f4aba24fed
Unknown <fabioadiranr2@gmail.com>
2019-02-23 17:23:12 -0300

    rank

--- app/src/main/java/entidades/usuario.java
+++ app/src/main/java/entidades/usuario.java
@@ -6,19 +6,25 @@ public class usuario {
     private String nombre;
     private String contraseña;
     private int puntos;
+    private int puntosM;
+    private int puntosP;
 
 
     public usuario(){
         this.id = null;
         this.nombre = "";
         this.contraseña = "";
-        this.puntos = 1000;
+        this.puntos=1000;
+        this.puntosM = 1000;
+        this.puntosP= 1000;
     }
-    public usuario(Integer id, String nombre, String contraseña, int puntaje) {
+    public usuario(Integer id, String nombre, String contraseña, int puntaje, int m, int p) {
         this.id = id;
         this.nombre = nombre;
         this.contraseña = contraseña;
         this.puntos = puntaje;
+        this.puntosM=m;
+        this.puntosP=p;
     }
 
     public Integer getId() {
@@ -48,4 +54,20 @@ public class usuario {
     public int getPuntaje () {return puntos; }
 
     public void setPuntaje(int puntaje){ this.puntos=puntaje; }
+
+    public int getPuntosM() {
+        return puntosM;
+    }
+
+    public void setPuntosM(int puntosM) {
+        this.puntosM = puntosM;
+    }
+
+    public int getPuntosP() {
+        return puntosP;
+    }
+
+    public void setPuntosP(int puntosP) {
+        this.puntosP = puntosP;
+    }
 }
